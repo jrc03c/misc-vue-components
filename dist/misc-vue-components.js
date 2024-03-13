a380e35a8dd63a7d2df693a488bc4ae51b92ee28
jrc03c <jrc03c@pm.me>
2024-03-13 14:50:00 -0500

    ...

--- dist/misc-vue-components.js
+++ dist/misc-vue-components.js
@@ -235,6 +235,10 @@
     gap: 0;
   }
 
+  .x-frame > *:not(.x-frame-divider) {
+    box-sizing: border-box !important;
+  }
+
   .x-frame.is-being-resized,
   .x-frame.is-being-resized * {
     user-select: none !important;
@@ -294,13 +298,13 @@
 `
       );
       var createVueComponentWithCSS = require_src();
-      function clamp(x, a, b) {
-        return x < a ? a : x > b ? b : x;
-      }
       function sum(x) {
         let s = 0;
         x.forEach((v) => s += v);
         return s;
+      }
+      function remap(x, a, b, c, d) {
+        return (d - c) * (x - a) / (b - a) + c;
       }
       module.exports = createVueComponentWithCSS({
         name: "x-frame",
@@ -326,6 +330,11 @@
             type: String,
             required: false,
             default: () => "horizontal"
+          },
+          "start-sizes": {
+            type: Array,
+            required: false,
+            default: () => null
           }
         },
         data() {
@@ -352,23 +361,41 @@
             if (this.isBeingResized) {
               if (this.orientation === "horizontal") {
                 const dx = event.pageX - this.mouse.x;
-                const children = Array.from(this.$el.children).filter(
-                  (child) => !child.classList.contains("x-frame-divider")
-                );
-                const child1 = children[this.activeDividerIndex];
-                const child2 = children[this.activeDividerIndex + 1];
+                const dividers = [];
+                const nonDividers = [];
+                Array.from(this.$el.children).forEach((child) => {
+                  if (child.classList.contains("x-frame-divider")) {
+                    dividers.push(child);
+                  } else {
+                    nonDividers.push(child);
+                  }
+                });
+                const child1 = nonDividers[this.activeDividerIndex];
+                const child2 = nonDividers[this.activeDividerIndex + 1];
                 const child1Rect = child1.getBoundingClientRect();
                 const child2Rect = child2.getBoundingClientRect();
-                this.widths[this.activeDividerIndex] = clamp(
-                  child1Rect.width + dx,
-                  this.minWidth,
-                  this.maxWidth
-                );
-                this.widths[this.activeDividerIndex + 1] = clamp(
-                  child2Rect.width - dx,
-                  this.minWidth,
-                  this.maxWidth
-                );
+                this.widths[this.activeDividerIndex] = child1Rect.width + dx;
+                this.widths[this.activeDividerIndex + 1] = child2Rect.width - dx;
+                if (this.widths[this.activeDividerIndex] < this.minWidth) {
+                  const delta = this.widths[this.activeDividerIndex] - this.minWidth;
+                  this.widths[this.activeDividerIndex] -= delta;
+                  this.widths[this.activeDividerIndex + 1] += delta;
+                }
+                if (this.widths[this.activeDividerIndex] > this.maxWidth) {
+                  const delta = this.widths[this.activeDividerIndex] - this.maxWidth;
+                  this.widths[this.activeDividerIndex] -= delta;
+                  this.widths[this.activeDividerIndex + 1] += delta;
+                }
+                if (this.widths[this.activeDividerIndex + 1] < this.minWidth) {
+                  const delta = this.widths[this.activeDividerIndex + 1] - this.minWidth;
+                  this.widths[this.activeDividerIndex + 1] -= delta;
+                  this.widths[this.activeDividerIndex] += delta;
+                }
+                if (this.widths[this.activeDividerIndex + 1] > this.maxWidth) {
+                  const delta = this.widths[this.activeDividerIndex + 1] - this.maxWidth;
+                  this.widths[this.activeDividerIndex + 1] -= delta;
+                  this.widths[this.activeDividerIndex] += delta;
+                }
                 this.updateStyles();
               } else {
               }
@@ -419,16 +446,61 @@
             });
             const parentWidth = parentRect.width - sum(dividers.map((d) => d.getBoundingClientRect().width));
             nonDividers.forEach((child, i) => {
-              child.style.width = `${this.widths[i] || parentWidth / nonDividers.length}px`;
+              const width = this.widths[i] || parentWidth / nonDividers.length;
+              this.widths[i] = width;
+              child.style.width = `${width}px`;
             });
           }
         },
         mounted() {
           this.observer = new MutationObserver(this.onMutation);
           this.observer.observe(this.$el, { childList: true });
-          this.onMutation();
           window.addEventListener("mousemove", this.onMouseMove);
           window.addEventListener("mouseup", this.onMouseUp);
+          this.$nextTick(() => {
+            if (this.startSizes) {
+              const parentRect = this.$el.getBoundingClientRect();
+              const startSizesArePercents = sum(this.startSizes) <= 1;
+              this.widths = this.startSizes.map((v) => {
+                if (startSizesArePercents) {
+                  return v * (this.orientation === "horizontal" ? parentRect.width : parentRect.height);
+                } else {
+                  return v;
+                }
+              });
+            }
+            this.onMutation();
+          });
+          const canvas = document.createElement("canvas");
+          canvas.style = /* css */
+          `
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100vw;
+      height: 10vh;
+    `;
+          const width = window.innerWidth;
+          const height = 0.1 * window.innerHeight;
+          canvas.width = width;
+          canvas.height = height;
+          document.body.appendChild(canvas);
+          const context = canvas.getContext("2d");
+          const loop = () => {
+            context.clearRect(0, 0, width, height);
+            const a = 360 / this.widths.length;
+            let x = 0;
+            const parentRect = this.$el.getBoundingClientRect();
+            const parentWidth = parentRect.width;
+            this.widths.forEach((w, i) => {
+              const tempWidth = remap(w, 0, parentWidth, 0, width);
+              context.fillStyle = `hsl(${i * a}, 100%, 50%)`;
+              context.fillRect(x, 0, tempWidth, height);
+              x += tempWidth;
+            });
+            window.requestAnimationFrame(loop);
+          };
+          loop();
         },
         unmounted() {
           this.observer.disconnect();
--- src/frame.js
+++ src/frame.js
@@ -13,6 +13,10 @@ const css = /* css */ `
     gap: 0;
   }
 
+  .x-frame > *:not(.x-frame-divider) {
+    box-sizing: border-box !important;
+  }
+
   .x-frame.is-being-resized,
   .x-frame.is-being-resized * {
     user-select: none !important;
@@ -89,6 +93,10 @@ function sum(x) {
   return s
 }
 
+function remap(x, a, b, c, d) {
+  return ((d - c) * (x - a)) / (b - a) + c
+}
+
 module.exports = createVueComponentWithCSS({
   name: "x-frame",
   template,
@@ -117,6 +125,12 @@ module.exports = createVueComponentWithCSS({
       type: String,
       required: false,
       default: () => "horizontal",
+    },
+
+    "start-sizes": {
+      type: Array,
+      required: false,
+      default: () => null,
     },
   },
 
@@ -147,27 +161,53 @@ module.exports = createVueComponentWithCSS({
         if (this.orientation === "horizontal") {
           const dx = event.pageX - this.mouse.x
 
-          const children = Array.from(this.$el.children).filter(
-            child => !child.classList.contains("x-frame-divider"),
-          )
+          const dividers = []
+          const nonDividers = []
 
-          const child1 = children[this.activeDividerIndex]
-          const child2 = children[this.activeDividerIndex + 1]
+          Array.from(this.$el.children).forEach(child => {
+            if (child.classList.contains("x-frame-divider")) {
+              dividers.push(child)
+            } else {
+              nonDividers.push(child)
+            }
+          })
+
+          const child1 = nonDividers[this.activeDividerIndex]
+          const child2 = nonDividers[this.activeDividerIndex + 1]
 
           const child1Rect = child1.getBoundingClientRect()
           const child2Rect = child2.getBoundingClientRect()
 
-          this.widths[this.activeDividerIndex] = clamp(
-            child1Rect.width + dx,
-            this.minWidth,
-            this.maxWidth,
-          )
+          this.widths[this.activeDividerIndex] = child1Rect.width + dx
+          this.widths[this.activeDividerIndex + 1] = child2Rect.width - dx
 
-          this.widths[this.activeDividerIndex + 1] = clamp(
-            child2Rect.width - dx,
-            this.minWidth,
-            this.maxWidth,
-          )
+          if (this.widths[this.activeDividerIndex] < this.minWidth) {
+            const delta = this.widths[this.activeDividerIndex] - this.minWidth
+            this.widths[this.activeDividerIndex] -= delta
+            this.widths[this.activeDividerIndex + 1] += delta
+          }
+
+          if (this.widths[this.activeDividerIndex] > this.maxWidth) {
+            const delta = this.widths[this.activeDividerIndex] - this.maxWidth
+            this.widths[this.activeDividerIndex] -= delta
+            this.widths[this.activeDividerIndex + 1] += delta
+          }
+
+          if (this.widths[this.activeDividerIndex + 1] < this.minWidth) {
+            const delta =
+              this.widths[this.activeDividerIndex + 1] - this.minWidth
+
+            this.widths[this.activeDividerIndex + 1] -= delta
+            this.widths[this.activeDividerIndex] += delta
+          }
+
+          if (this.widths[this.activeDividerIndex + 1] > this.maxWidth) {
+            const delta =
+              this.widths[this.activeDividerIndex + 1] - this.maxWidth
+
+            this.widths[this.activeDividerIndex + 1] -= delta
+            this.widths[this.activeDividerIndex] += delta
+          }
 
           this.updateStyles()
         } else {
@@ -235,7 +275,9 @@ module.exports = createVueComponentWithCSS({
         sum(dividers.map(d => d.getBoundingClientRect().width))
 
       nonDividers.forEach((child, i) => {
-        child.style.width = `${this.widths[i] || parentWidth / nonDividers.length}px`
+        const width = this.widths[i] || parentWidth / nonDividers.length
+        this.widths[i] = width
+        child.style.width = `${width}px`
       })
     },
   },
@@ -243,9 +285,68 @@ module.exports = createVueComponentWithCSS({
   mounted() {
     this.observer = new MutationObserver(this.onMutation)
     this.observer.observe(this.$el, { childList: true })
-    this.onMutation()
     window.addEventListener("mousemove", this.onMouseMove)
     window.addEventListener("mouseup", this.onMouseUp)
+
+    this.$nextTick(() => {
+      if (this.startSizes) {
+        const parentRect = this.$el.getBoundingClientRect()
+        const startSizesArePercents = sum(this.startSizes) <= 1
+
+        this.widths = this.startSizes.map(v => {
+          if (startSizesArePercents) {
+            return (
+              v *
+              (this.orientation === "horizontal"
+                ? parentRect.width
+                : parentRect.height)
+            )
+          } else {
+            return v
+          }
+        })
+      }
+
+      this.onMutation()
+    })
+
+    const canvas = document.createElement("canvas")
+
+    canvas.style = /* css */ `
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100vw;
+      height: 10vh;
+    `
+
+    const width = window.innerWidth
+    const height = 0.1 * window.innerHeight
+    canvas.width = width
+    canvas.height = height
+    document.body.appendChild(canvas)
+
+    const context = canvas.getContext("2d")
+
+    const loop = () => {
+      context.clearRect(0, 0, width, height)
+
+      const a = 360 / this.widths.length
+      let x = 0
+      const parentRect = this.$el.getBoundingClientRect()
+      const parentWidth = parentRect.width
+
+      this.widths.forEach((w, i) => {
+        const tempWidth = remap(w, 0, parentWidth, 0, width)
+        context.fillStyle = `hsl(${i * a}, 100%, 50%)`
+        context.fillRect(x, 0, tempWidth, height)
+        x += tempWidth
+      })
+
+      window.requestAnimationFrame(loop)
+    }
+
+    loop()
   },
 
   unmounted() {
